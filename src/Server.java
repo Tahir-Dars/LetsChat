2e6967e226f90361821e28c4efb0e7bf65cb44d2
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-06-25 22:40:37 +0500

    Reomved the main class and added the implementation of Server class

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    Socket socket;
+
+    public Server(Socket socket) {
+        try {
+            this.socket = socket;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void main() {
+        try (ServerSocket serverSocket = new ServerSocket(2003);) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Server server = new Server(socket);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
